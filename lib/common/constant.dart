439d106228d77b9abeaaf5e2644030f42ce326f2
firman-ali <firmanali22@gmail.com>
2021-12-01 19:05:41 +0800

    add constant variable

--- lib/common/constant.dart
+++ lib/common/constant.dart
@@ -1,5 +1,8 @@
 const appTitle = 'Kuaca Bali';
 const appCaption = '\nTemukan Busana Adat Impianmu Dengan Lebih Mudah Disini';
 const welcomeImageAsset = 'assets/images/undraw_jewelry_iima.svg';
+const loginImageAsset = 'assets/images/Mobile login-amico.svg';
 const registerButtonTitle = 'Daftar Akun';
 const loginButtonTitle = 'Login Akun';
+const emailLabel = 'Email';
+const passLabel = 'Password';
