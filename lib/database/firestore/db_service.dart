afc244ab121be9882b11b2894f755825e2eaa0ac
firman-ali <firmanali22@gmail.com>
2021-12-07 17:50:04 +0800

    add features in database service

--- lib/database/firestore/db_service.dart
+++ lib/database/firestore/db_service.dart
@@ -1,23 +1,63 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:kuaca_bali/model/detail_data.dart';
-import 'package:kuaca_bali/model/list_data.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:kuaca_bali/database/auth/auth_service.dart';
+import 'package:kuaca_bali/model/add_data_model.dart';
+import 'package:kuaca_bali/model/detail_data_model.dart';
+import 'package:kuaca_bali/model/list_data_model.dart';
+import 'package:path/path.dart';
 
 class DatabaseService {
-  final _collection = FirebaseFirestore.instance.collection('dress');
+  final _collection = FirebaseFirestore.instance.collection('dresses');
 
   Future<List<DressData>> getListData() async {
     final snapshot = await _collection.get();
     List<DressData> _dressList = [];
 
-    _dressList = snapshot.docs.map((e) => DressData.fromObject(e)).toList();
+    for (var e in snapshot.docs) {
+      final seller = await AuthService().getUserDetail(e['sellerId']);
+      final data = DressData.fromObject(e, seller!);
+      _dressList.add(data);
+    }
 
     return _dressList;
   }
 
-  Future<DressDataElement> geDataById(String id) async {
+  Future<DressDataElement> geDetailData(String id) async {
     final snapshot = await _collection.doc(id).get();
-    DressDataElement _dressDetail = DressDataElement.fromObject(snapshot);
+    final seller = await AuthService().getUserDetail(snapshot['sellerId']);
+    DressDataElement _dressDetail =
+        DressDataElement.fromObject(snapshot, seller!);
 
     return _dressDetail;
+  }
+
+  Future<String> inputData(String dressName, int price, String description,
+      String size, File imageFile) async {
+    final dateNow = Timestamp.fromDate(DateTime.now());
+    final sellerId = AuthService().getUserId();
+    final imageUrl = await _uploadDressImage(imageFile);
+    final data = AddDressData(
+      dressName: dressName,
+      description: description,
+      imageUrl: imageUrl,
+      price: price,
+      size: size,
+      sellerId: sellerId,
+      createdAt: dateNow,
+      updatedAt: dateNow,
+    );
+    await _collection.add(data.toObject());
+    return "success";
+  }
+
+  Future<String> _uploadDressImage(File _imageFile) async {
+    String fileName = basename(_imageFile.path);
+    Reference firebaseStorageRef =
+        FirebaseStorage.instance.ref().child('images/dresses/$fileName');
+    final uploadTask = await firebaseStorageRef.putFile(_imageFile);
+    final url = await uploadTask.ref.getDownloadURL();
+    return url;
   }
 }
