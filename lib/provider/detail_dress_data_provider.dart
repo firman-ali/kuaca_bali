90906c7bf25ccd77b7b22e23845de9300f22d41c
firman-ali <firmanali22@gmail.com>
2021-12-10 10:02:14 +0800

    add detail data provider

---
+++ lib/provider/detail_dress_data_provider.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:kuaca_bali/database/firestore/db_service.dart';
+import 'package:kuaca_bali/helper/state_helper.dart';
+import 'package:kuaca_bali/model/detail_data_model.dart';
+import 'package:kuaca_bali/model/user_data_model.dart';
+
+class ListDataProvider extends ChangeNotifier {
+  DatabaseService dbService;
+  String dressId;
+  ListDataProvider({
+    required this.dbService,
+    required this.dressId,
+  }) {
+    _fetchData();
+  }
+
+  late DressDataElement _dressData;
+  late ResultState _state;
+  late UserData _user;
+
+  UserData get user => _user;
+  ResultState get state => _state;
+  DressDataElement get data => _dressData;
+
+  _fetchData() async {
+    _state = ResultState.isLoading;
+    notifyListeners();
+    try {
+      final snapshot = await dbService.geDetailData(dressId);
+      _dressData = snapshot;
+      _state = ResultState.hasData;
+    } catch (e) {
+      _state = ResultState.isError;
+    }
+    notifyListeners();
+  }
+}
