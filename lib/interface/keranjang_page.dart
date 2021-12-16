13f7620ea14296fd7a029ef25e2f000ce72f35e9
Rezaseptiawan <maderezaseptiawan@gmail.com>
2021-12-16 22:15:33 +0800

    Keranjang page minus icon

--- lib/interface/keranjang_page.dart
+++ lib/interface/keranjang_page.dart
@@ -39,27 +39,61 @@ class KeranjangPage extends StatelessWidget {
                 ],
               ),
             ),
-            SizedBox(height: 25.0),
-            Column(
-              children: [
-                Row(
-                  children: [
-                    Image.network(
-                      'https://images.unsplash.com/photo-1614772903208-613ed4282ded?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
-                      width: 130.0,
-                      height: 130.0,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [Icon(Icons.store)],
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ],
+            Container(
+              padding: EdgeInsets.all(10.0),
+              height: 350,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Image.network(
+                        'https://images.unsplash.com/photo-1614772903208-613ed4282ded?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
+                        width: 130.0,
+                        height: 130.0,
+                      ),
+                      SizedBox(width: 10),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.access_time,
+                                size: 25,
+                              ),
+                              Text(
+                                '1/01/2021 - 4/01/2021',
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            'Nama Busana Adat',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 25),
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.store,
+                                size: 25,
+                              ),
+                              Text(
+                                'Nama Toko',
+                                style: TextStyle(fontSize: 20),
+                              )
+                            ],
+                          ),
+                          Text(
+                            'Rp.3,200.000',
+                            style: TextStyle(fontSize: 25),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             )
           ],
         ),
